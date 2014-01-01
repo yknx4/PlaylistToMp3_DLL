80a598924021a7979b24d698ab38687f222fef60
Jorge Figueroa (Qosmio W8) <yknx4@outlook.com>
2013-12-31 20:42:26 -0600

    Initial Commit

---
+++ MusicFile.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+
+namespace PlaylistToMp3_DLL
+{
+    public delegate void ConvertedEventHandler(object sender, EventArgs e);
+    public class MusicFile
+    {
+        private TagLib.File _file;
+
+        public event ConvertedEventHandler Converted;
+        private bool _isConverted;
+
+        protected virtual void OnConverted(EventArgs e)
+        {
+            if (Converted != null)
+            {
+                Converted(this, e);
+            }
+        }
+
+        public MusicFile(TagLib.File file)
+        {
+            _file = file;
+        }
+        [Browsable(false)]
+        public FileInfo FileInformation
+        {
+            get
+            {
+                return new FileInfo(FileName);
+            }
+        }
+        [Browsable(false)]
+        public string FileName { get { return _file.Name; } }
+        [DisplayName("File Name")]
+        public string ShortFileName { get { return FileInformation.Name; } }
+        public string Format { get { return (new FileInfo(_file.Name)).Extension; } }
+        public string Artist
+        {
+            get
+            {
+                try
+                {
+                    return _file.Tag.Performers.First() != null ? _file.Tag.Performers.First().ToString() : "";
+                }
+                catch (Exception)
+                {
+                    return "No AArtist";
+                }
+            }
+        }
+        public string Album
+        {
+            get
+            {
+                try
+                {
+                    return validateString(_file.Tag.Album.ToString());
+                }
+                catch (Exception)
+                {
+                    return "No Album";
+                }
+
+            }
+        }
+        private string validateString(string input)
+        {
+            if (input != null)
+            {
+                return input;
+            }
+            return string.Empty;
+        }
+        public string Title
+        {
+            get
+            {
+                try { return validateString(_file.Tag.Title.ToString()); }
+                catch (Exception) { return ShortFileName; }
+
+            }
+        }
+        public TimeSpan Duration
+        {
+            get
+            {
+                if (_file.Properties.MediaTypes != TagLib.MediaTypes.None)
+                    return _file.Properties.Duration;
+                return new TimeSpan();
+            }
+        }
+        public int Bitrate
+        {
+            get
+            {
+                foreach (TagLib.ICodec codec in _file.Properties.Codecs)
+                {
+                    TagLib.IAudioCodec acodec = codec as TagLib.IAudioCodec;
+                    TagLib.IVideoCodec vcodec = codec as TagLib.IVideoCodec;
+                    if (acodec != null && (acodec.MediaTypes & TagLib.MediaTypes.Audio) != TagLib.MediaTypes.None)
+                    {
+                        Console.WriteLine("Audio Properties : " + acodec.Description);
+                        return acodec.AudioBitrate;
+                    }
+                }
+                return 0;
+            }
+        }
+
+        public int Progress
+        {
+            get;
+            set;
+        }
+        [DisplayName("Converted")]
+        public bool isConverted
+        {
+            get
+            {
+                return _isConverted;
+            }
+            set
+            {
+                OnConverted(EventArgs.Empty);
+                _isConverted = value;
+            }
+        }
+        public void resetEvents()
+        {
+            Converted = null;
+        }
+
+    }
+}
---
+++ PlaylistLoader.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using TagLib;
+
+
+
+namespace PlaylistToMp3_DLL
+{
+    /// <summary>
+    /// Class for loading the .m3u8 playlist.
+    /// </summary>
+public static class PlaylistLoader
+    {
+        private static string _error;
+        /// <summary>
+        /// Gets the error.
+        /// </summary>
+        /// <value>
+        /// The error.
+        /// </value>
+        public static String Error { get { return _error != null ? _error : ""; } }
+        /// <summary>
+        /// Loads the specified path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>
+        /// A list with all music files within the list.
+        /// </returns>
+        /// 
+        public static List<MusicFile> GetPlaylist(string path)
+        {
+            var result = new List<MusicFile>();
+            foreach (var entry in Load(path))
+            {
+                result.Add(new MusicFile(entry));
+            }
+            return result;
+        }
+        public static List< TagLib.File> Load(string path)
+        {
+            var result = new List< TagLib.File>();
+            try
+            {
+                foreach (string entry in System.IO.File.ReadAllLines(path))
+                {
+                    string entry_f = entry;
+                    if (entry[1] != ':')
+                    {
+                        entry_f = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) , entry_f);
+                        
+                    }
+                    FileInfo m_Entry = new FileInfo(Path.GetFullPath(entry_f));
+                    TagLib.File file = null;
+                    if (m_Entry.Exists)
+                    {
+                        try
+                        {
+                            file = TagLib.File.Create(m_Entry.FullName);
+                        }
+                        catch (TagLib.UnsupportedFormatException)
+                        {
+                            Console.WriteLine("UNSUPPORTED FILE: " + m_Entry.FullName);
+                            Console.WriteLine(String.Empty);
+                            Console.WriteLine("---------------------------------------");
+                            Console.WriteLine(String.Empty);
+                            continue;
+                        }
+                        result.Add(file);
+                    }
+                    else
+                    {
+                        throw new ArgumentException(m_Entry.FullName);
+                    }
+
+
+                }
+            }
+
+            catch (SystemException ex)
+            {
+                _error = ex.ToString();
+                //throw ex;
+            }
+
+            return result;
+        }
+    }
+}
